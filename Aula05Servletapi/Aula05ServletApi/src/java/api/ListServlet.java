0c5b1319947a1e21c2741b407c85bfb7f776ee79
renanitos <60163031+renanitos@users.noreply.github.com>
2022-09-12 22:10:30 -0300

    Add files via upload

---
+++ Aula05Servletapi/Aula05ServletApi/src/java/api/ListServlet.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import org.json.*;
+
+/**
+ *
+ * @author Fatec
+ */ 
+@WebServlet(name = "ListServlet", urlPatterns = {"/api/list.json"})
+public class ListServlet extends HttpServlet {
+    
+    private static ArrayList<String> arrayList = new ArrayList<>();
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if(request.getParameter("add") != null){
+            arrayList.add(request.getParameter("add"));
+        }
+        if(request.getParameter("remove") != null){
+            arrayList.remove(request.getParameter("remove"));
+        }
+        response.setContentType("application/json;charset=UTF-8");
+        JSONObject file = new JSONObject();
+        file.put("nome", "list");
+        file.put("size", arrayList.size());
+        file.put("array", arrayList);
+        
+        response.getWriter().println(file.toString());
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
